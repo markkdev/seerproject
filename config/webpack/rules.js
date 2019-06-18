d0cb66ae3bbae454182c4fe83a733eaa506c74a0
Mark Khuzam <khuzammark@gmail.com>
2019-06-17 21:51:08 -0700

    updated webpack config

--- config/webpack/rules.js
+++ config/webpack/rules.js
@@ -7,26 +7,14 @@ module.exports = [
         }
     },
     {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        exclude: /node_modules/,
-        loader: 'file-loader'
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader',
+        options: {
+            limit: 10000
+        }
     },
     {
-        test: /\.(woff|woff2)$/,
-        exclude: /node_modules/,
-        loader: 'url-loader?prefix=font/&limit=5000'
-    },
-    {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        exclude: /node_modules/,
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
-    },
-    {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        use: ['url-loader?limit=10000', 'img-loader']
-    },
-    {
-        test: /\.s(a|c)ss$/,
+        test: /\.(s?)css$/,
         use: [
             {
                 loader: 'style-loader'
