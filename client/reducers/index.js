e0f4ac5a6c3be04807149b465d403016cd17f88c
Mark Khuzam <mark@markk.dev>
2019-11-20 18:00:58 -0800

    edited eslint prettier rcs

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,14 +1,27 @@
 const initialState = {
-    name: ''
+  firstName: '',
+  lastName: '',
+  username: '',
+  email: '',
+  password: '',
+  confirmPassword: ''
 };
+/**
+- First Name
+- Last Name
+- Username
+- Email
+- Password
+- Confirm Password
+ */
 
 const rootReducer = (state = initialState, { type, payload }) => {
-    switch (type) {
-        case 'SUBMIT_FORM':
-            return { ...state, ...payload };
-        default:
-            return state;
-    }
+  switch (type) {
+    case 'SUBMIT_FORM':
+      return { ...state, ...payload };
+    default:
+      return state;
+  }
 };
 
 export default rootReducer;
